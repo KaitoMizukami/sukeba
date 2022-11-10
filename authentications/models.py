22cf9260607fbd2e55393c7856e7356cb2b00c0a
KaitoMizukami <kaito.program@gmail.com>
2022-11-11 00:35:39 +0900

    feat: make a user managing class
    Make UserManage class and commands becasuse custom user class needs to specify it

--- authentications/models.py
+++ authentications/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
 )
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('メールアドレスは必須です')
+        if not username:
+            raise ValueError('ユーザーネームは必須です')
+        
+        email = self.normalize_email(email) # ドメイン部分を小文字にして、メールアドレスを正規化
+        user = self.model(
+            username=username,
+            email=email,  
+        )
+        user.set_password(password) # ハッシュ化されたパスワードを作成
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        super_user = self.create_user(email, username, password)
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.save(using=self._db)
+        return super_user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -18,8 +43,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email' # Userモデルを一意に識別するフィールド
     REQUIRED_FIELDS = ['username'] # createsuperuserコマンドでユーザー作成する時の追加フィールド
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
