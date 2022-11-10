a89c33d4a6b801d8845f03b6a909a5874b602a2c
KaitoMizukami <kaito.program@gmail.com>
2022-11-11 00:23:04 +0900

    feat(model): make custom user class
    To make own custom user, added two build-in class and inherit it.
    Becase PermissionsMixin has already has password field so I didn't add it in our user model

--- authentications/models.py
+++ authentications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        max_length=250, unique=True,
+        verbose_name='メールアドレス'
+    )
+    username = models.CharField(max_length=100, verbose_name='ユーザーネーム')
+    instagram_username = models.CharField(
+        max_length=100, blank=True, null=True,
+        verbose_name='インスタグラムのユーザーネーム',
+        help_text='任意です'
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email' # Userモデルを一意に識別するフィールド
+    REQUIRED_FIELDS = ['username'] # createsuperuserコマンドでユーザー作成する時の追加フィールド
+
+    def __str__(self):
+        return self.username
