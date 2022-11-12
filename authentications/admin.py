73aa75bf1e41178d5862f503ffd85edca69dfd71
KaitoMizukami <kaito.program@gmail.com>
2022-11-12 15:47:03 +0900

    feat: Make CustomUserAdmin class
    To display custom user model on admin site, override some fields

--- authentications/admin.py
+++ authentications/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # ユーザーインスタンスを追加、変更するためのフォーム
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # ユーザーモデルの表示に使用されるフィールド
+    list_display = ('email', 'username', 'is_staff')
+    fieldsets = (
+        ('ユーザー情報', {'fields': (
+            'username', 'email', 'password', 'instagram_username'
+        )}),
+        ('パーミッション', {'fields': (
+            'is_active', 'is_staff', 'is_superuser'
+        )})
+    )
+
+    # ユーザーインスタンスを作成する時に必要なフィールド
+    add_fieldsets = (
+        ('ユーザユーザー情報', {'fields': (
+            'username','email', 'password', 'confirm_password'
+        )})
+    )
