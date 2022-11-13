35daf2099020982455321204a8a147aade2060d4
KaitoMizukami <kaito.program@gmail.com>
2022-11-14 01:25:11 +0900

    chore: add posts app into INSTALLED_APPS

--- sukeba/settings.py
+++ sukeba/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentications.apps.AuthenticationsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
