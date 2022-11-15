a8a3740e1038ba192988bea83f34420d0e381180
KaitoMizukami <kaito.program@gmail.com>
2022-11-16 02:06:58 +0900

    feat: make PostsListView
    To list all posts data, inherit ListView

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class PostsListView(ListView):
+    """
+    全ての投稿データを取得し,対応したHTMLに渡す
+    """
+    template_name = 'posts/posts_list.html'
+    model = Post
