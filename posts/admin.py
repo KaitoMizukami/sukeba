0e6afa19de4c659321094aeff83bf823a8bba444
KaitoMizukami <kaito.program@gmail.com>
2022-11-15 11:41:54 +0900

    feat: make two admin models

--- posts/admin.py
+++ posts/admin.py
@@ -3,5 +3,38 @@ from django.contrib import admin
 from .models import Post, Location
 
 
-admin.site.register(Post)
-admin.site.register(Location)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # 投稿モデルの表示に使用されるフィールド
+    list_display = ('author', 'location', 'body',)
+    fieldsets = (
+        ('投稿', {
+            'fields': (
+                'author', 'location', 'body',
+            )
+        }),
+    )
+     # 1つのフィールドでユーザーを絞り込む
+    list_filter = ('author',)
+
+    # 検索される時に使われるフィールド
+    search_fields = ('author', 'location',)
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prefecture', 'city')
+    fieldsets = (
+        ('スケートパーク', {
+            'fields': (
+                'name', 
+                'prefecture',
+                'city',
+                'location_image',
+            )
+        }),
+    )
+    # 2つのフィールドでユーザーを絞り込む
+    list_filter = ('prefecture', 'city',)
+
+    # 検索される時に使われるフィールド
+    search_fields = ('name',)
