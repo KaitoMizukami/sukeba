12411df33f5443a30f1cb9fb29bd2a10c252d7b9
KaitoMizukami <kaito.program@gmail.com>
2022-11-17 14:33:44 +0900

    feat: override get_queryset in PostsListView
    Add query search function in prefectures.html, cliked prefecture will be
    sent to PostsListView and filter post model by query parameter

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Q
 
 from .models import Post
 from .prefectures import PREFECTURE_CHOICES
@@ -12,8 +13,27 @@ class PostsListView(ListView):
     template_name = 'posts/posts_list.html'
     model = Post
 
+    def get_queryset(self, **kwargs):
+        """ 
+        デフォルトでPostモデルの全てのデータをリストで返す
+        queryキーワードがある場合はマッチしたPostモデルのデータをリストで返す
+        """
+        queryset = super().get_queryset(**kwargs)
+        # GETリクエストパラメータにqueryがあれば、それでフィルタする
+        query_keyword = self.request.GET.get('query')
+        if query_keyword:
+            # locationモデルのprefectureとquery_keywordが一致するデータをフィルタする
+            queryset = Post.objects.filter(
+                Q(location__prefecture__contains=query_keyword)
+            )
+        return queryset
+
     def get_context_data(self, **kwargs):
+        """
+        テンプレートに渡すコンテキストにデータを加える
+        """
         context = super().get_context_data(**kwargs)
+        # 全都道府県のリストをコンテキストに追加
         context['prefectures'] = PREFECTURE_CHOICES
         return context
 
