57d0f95d9f189fc1533ee6531d9d67ab6d7ba139
KaitoMizukami <kaito.program@gmail.com>
2022-11-19 16:24:40 +0900

    feat: add urlConf to PostsDetailView

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.PostsListView.as_view(), name='list'),
-    path('posts/create/', views.PostsCreateView.as_view(), name='create')
+    path('posts/create/', views.PostsCreateView.as_view(), name='create'),
+    path('posts/detail/<int:pk>', views.PostsDetailView.as_view(), name='detail')
 ]
