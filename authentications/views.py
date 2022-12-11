56a3dc92e43e27e1094d4bbcb6ba49f6848925c6
KaitoMizukami <kaito.program@gmail.com>
2022-12-12 00:28:36 +0900

    fix: change the html file typo

--- authentications/views.py
+++ authentications/views.py
@@ -5,7 +5,7 @@ from .forms import UserCreationForm
 
 
 class AuthenticationsSignup(FormView):
-    template_name = 'authentications/authentications_login.html'
+    template_name = 'authentications/authentications_signup.html'
     form_class = UserCreationForm
 
     
