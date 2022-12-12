72d8fe572d4548bd30c8cadbc61e32af971836c6
KaitoMizukami <kaito.program@gmail.com>
2022-12-12 20:32:13 +0900

    feat: add function that creates new user
    After success creating new user, it automatically login by new user

--- authentications/views.py
+++ authentications/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
+from django.contrib.auth import authenticate, login
 
 from .forms import UserCreationForm
 
@@ -8,4 +9,19 @@ class AuthenticationsSignup(FormView):
     template_name = 'authentications/authentications_signup.html'
     form_class = UserCreationForm
 
-    
+    def post(self, request, *args, **kwargs):
+        """ 
+        Postリクエスト時の処理
+        Userモデルのフォームを検証し、データを保存する
+        検証成功すれば投稿一覧ページにリダイレクトし、自動でログインした状態にする
+        検証失敗すれば登録ページを返す
+        """
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            user = authenticate(email=user_form.cleaned_data['email'], password=user_form.cleaned_data['password'])
+            login(request, user)
+            return redirect('posts:list')
+        else:
+            return redirect('authentications:signup')
+
