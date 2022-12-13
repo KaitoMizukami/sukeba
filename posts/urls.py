43a2b2985764a41c9d7398c19f0b1201e6599416
KaitoMizukami <kaito.program@gmail.com>
2022-12-13 19:39:50 +0900

    feat: add url to delete view

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.PostsListView.as_view(), name='list'),
     path('posts/create/', views.PostsCreateView.as_view(), name='create'),
-    path('posts/detail/<int:pk>', views.PostsDetailView.as_view(), name='detail')
+    path('posts/detail/<int:pk>', views.PostsDetailView.as_view(), name='detail'),
+    path('posts/delete/<int:pk>', views.PostsDeleteView.as_view(), name='delete'),
 ]
