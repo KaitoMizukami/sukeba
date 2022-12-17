893c3943c49aabbd230159abbdb70dbe7090fb6b
KaitoMizukami <kaito.program@gmail.com>
2022-12-17 18:20:13 +0900

    refactor: make a variable for duplicated part

--- authentications/tests/test_views.py
+++ authentications/tests/test_views.py
@@ -13,42 +13,47 @@ class AuthenticationsSignupViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.signup_template = 'authentications/authentications_signup.html'
+        self.credentials = {
+            'email': 'test@mail.com', 'password': 'testpassword'
+        }
+        self.url_name = 'authentications:signup'
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('authentications:signup'))
+        response = self.client.get(reverse(self.url_name))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('authentications:signup'))
-        self.assertTemplateUsed(response, 'authentications/authentications_signup.html')
+        response = self.client.get(reverse(self.url_name))
+        self.assertTemplateUsed(response, self.signup_template)
 
     def test_view_creates_a_new_user(self):
         before_count = User.objects.all().count
-        response = self.client.post(reverse('authentications:signup'), {
+        response = self.client.post(reverse(self.url_name), {
             'username': 'testuser', 'email': 'test@mail.com',
             'password': 'testpassword', 'confirm_password': 'testpassword'
         }, follow=True)
-        after_count = User.objects.all().count
         self.assertEqual(response.status_code, 200)
+        after_count = User.objects.all().count
         self.assertNotEqual(before_count, after_count)
     
     def test_authenticated_user_redirect_to_main_page(self):
-        _ = self.client.post(reverse('authentications:login'), {
-            'email': 'test@mail.com', 'password': 'testpassword'
-        })
-        response = self.client.get(reverse('authentications:signup'))
+        _ = self.client.post(reverse('authentications:login'), self.credentials)
+        response = self.client.get(reverse(self.url_name))
+        # リダイレクト(302)が発生したかテスト
         self.assertEqual(response.status_code, 302)
-        response = self.client.get(reverse('authentications:signup'), follow=True)
-        self.assertTemplateNotUsed(response, 'authentications/authentications_signup.html')
+        response = self.client.get(reverse(self.url_name), follow=True)
+        # リダイレクト先のHTMLファイルを確認
+        self.assertTemplateNotUsed(response, self.signup_template)
         self.assertTemplateUsed(response, 'posts/posts_list.html')
 
     def test_view_render_signup_page_if_form_is_invalid(self):
-        response = self.client.post(reverse('authentications:signup'), {})
-        self.assertTemplateUsed(response, 'authentications/authentications_signup.html')
+        response = self.client.post(reverse(self.url_name), {})
+        self.assertTemplateUsed(response, self.signup_template)
 
 
 class AuthenticationsLoginViewTest(TestCase):
