82af86ebee0208c3e8a80a188d9352c320325ea7
KaitoMizukami <kaito.program@gmail.com>
2022-12-17 21:53:12 +0900

    test: create a test methods for userprofile view

--- authentications/tests/test_views.py
+++ authentications/tests/test_views.py
@@ -112,3 +112,65 @@ class AuthenticationsLoginViewTest(TestCase):
             'email': 'aaaaa@mail.com', 'password': 'aaaaaaaaa'
         }, follow=True)
         self.assertTemplateUsed(response, self.login_template)
+
+
+class AuthenticationsLogoutViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='testuser', email='test@mail.com')
+        user.set_password('testpassword')
+        user.save()
+
+    def setUp(self):
+        self.client = Client()
+        self.credentials = {
+            'email': 'test@mail.com', 'password': 'testpassword'
+        }
+        self.url_name = 'authentications:logout'
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/accounts/logout/')
+        # ログアウトはテンプレートはなくリダイレクト(302)する
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse(self.url_name))
+        # ログアウトはテンプレートはなくリダイレクト(302)する
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_can_logout(self):
+        response = self.client.post(reverse('authentications:login'), self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+        response = self.client.get(reverse('authentications:logout'), follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+
+
+class UserProfileViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='testuser', email='test@mail.com')
+        user.set_password('testpassword')
+        user.save()
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.get(email='test@mail.com')
+        self.credentials = {
+            'email': 'test@mail.com', 'password': 'testpassword'
+        }
+        self.template_name = 'authentications/user_profile.html'
+        self.url_name = 'authentications:profile'
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(f'/accounts/profile/{self.user.pk}')
+        self.assertEqual(response.status_code, 200)
+                                  
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse(self.url_name, kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse(self.url_name, kwargs={'pk': self.user.pk}))
+        self.assertTemplateUsed(response, self.template_name)
+    
+    
